31a199adb0decdc79fd051e998c8fe6379450abb
Utkarsh1812 <112841675+Utkarsh1812@users.noreply.github.com>
2022-11-21 10:08:28 +0530

    add number

---
+++ src/jUnitTestPackage/jUnitFunction.java
@@ -0,0 +1,17 @@
+package jUnitTestPackage;
+
+public class jUnitFunction {
+	
+	public static int addNumber(int a, int b) {
+		int sum = a+b;
+		return sum;
+		
+	}
+	public static String addString(String x, String y) {
+		String z = x+y;
+		return z;
+
+}
+	public static void main (String arg[]) {
+	}
+}
